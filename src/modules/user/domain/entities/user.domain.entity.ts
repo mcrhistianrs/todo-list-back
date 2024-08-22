9516f63d19be71ecff6bc96fb740c5d73e7f0ee1
Maicon Crhistian <maicon.crhistian@eclipseworks.com.br>
2024-08-22 10:54:05 -0300

    chore: its  has been started the prisma configuration

---
+++ src/modules/user/domain/entities/user.domain.entity.ts
@@ -0,0 +1,34 @@
+export type UserDomainEntityInput = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  created_at: Date;
+};
+
+class UserDomainEntity {
+  private constructor(private field: UserDomainEntityInput) {}
+  static create(
+    name: string,
+    email: string,
+    password: string,
+  ): UserDomainEntity {
+    return new UserDomainEntity({
+      id: crypto.randomUUID().toString(),
+      name,
+      email,
+      password,
+      created_at: new Date(),
+    });
+  }
+  static load(input: UserDomainEntityInput): UserDomainEntity {
+    return new UserDomainEntity(input);
+  }
+  getName(): string {
+    return this.field.name;
+  }
+  getEmail(): string {
+    return this.field.email;
+  }
+}
+export { UserDomainEntity };
