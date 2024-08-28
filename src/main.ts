69bf764c819398aef3b1cbff0c86abd97825a648
Maicon Crhistian <maicon.crhistian@eclipseworks.com.br>
2024-08-28 15:06:06 -0300

    chore: cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     //origin: 'http://localhost:5173', // Your frontend URL
-    origin: '*',
+    origin:
+      'https://todo-list-front-git-main-mcrhistianrs-projects.vercel.app/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // If you need to handle cookies
   });
