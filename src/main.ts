639e7a9080930ccc21dd1ffbe6e5e40d0df537cd
Maicon Crhistian <maicon.crhistian@eclipseworks.com.br>
2024-08-28 14:38:31 -0300

    chore: cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173', // Your frontend URL
+    //origin: 'http://localhost:5173', // Your frontend URL
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // If you need to handle cookies
   });
